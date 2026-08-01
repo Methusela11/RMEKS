998894c1d89aec899e9449287e7d323509345377
Methusela11 <soenkilifi@gmail.com>
2026-08-02 00:45:17 +0300

    updated services

--- methusela/settings.py
+++ methusela/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # serve static files in production
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
